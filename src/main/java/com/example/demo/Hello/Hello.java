027023dba46a5e279d5b025691d3ed7e0b7c7b49
Noah da Silveira <noah.da.silveira@securex.be>
2026-03-11 07:18:49 +0100

    Learning Day 1

---
+++ src/main/java/com/example/demo/Hello/Hello.java
@@ -0,0 +1,27 @@
+package com.example.demo.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World, this will be a spring boot application";
+    }
+
+
+    @GetMapping("/countStringCharacters")
+    public String countStringCharacters(String givenString) {
+        int count = givenString.length();
+        if (count >= 1) {
+            System.out.printf("\"%s\" has %d characters.", givenString, count);
+            return givenString + " " + "has " + count + " characters";
+
+        } else  {
+            System.out.println("No string was given.");
+            return "No string was given.";
+        }
+    }
+}
